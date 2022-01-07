d257e33d8a1691ca5d261fa2403085255252514b
Marco <marcoscale98libero.it>
2022-01-07 15:15:37 +0100

    initial commit

---
+++ microservizio1/src/test/java/com/example/microservizio1/Microservizio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservizio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservizio1ApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
---
+++ microservizio3/src/main/java/com/example/microservizio3/Microservizio3Application.java
@@ -0,0 +1,13 @@
+package com.example.microservizio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservizio3Application {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Microservizio3Application.class, args);
+   }
+
+}
